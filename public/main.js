89022f500bf79fb8fcc1f171867c304c595cc3db
mielmaswary <miel1983@gmail.com>
2021-03-03 17:01:06 +0200

    basic slides

---
+++ public/main.js
@@ -0,0 +1,5 @@
+const booksImgSrc=[
+ 'https://images-evrit.yit.co.il/Images/Products/NewBO/StripItems/10588/1100X345_gambitnew.jpg',
+ 'https://images-evrit.yit.co.il/Images/Products/NewBO/StripItems/10377/1100X345_makor21.jpg',
+ 'https://images-evrit.yit.co.il/Images/Products/NewBO/StripItems/10352/1100X345_8BookFEBnew.jpg'
+]
