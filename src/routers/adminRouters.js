446479f1afa64c3378ce7b37c5d62cf90b5749f7
mielmaswary <miel1983@gmail.com>
2021-04-25 21:28:19 +0300

    try3

--- src/routers/adminRouters.js
+++ src/routers/adminRouters.js
@@ -1,38 +1,38 @@
-const express=require('express')
-const auth = require("../middleware/auth");
+// const express=require('express')
+// const auth = require("../middleware/auth");
 
-const Admin = require('../models/adminModel')
-const Book =require('../models/bookModel')
-const router=new express.Router()
-
-
-router.post("/admin/add" , async (req, res) => {
-    const admin = new Admin(req.body);
-	console.log(admin)
-	try {
-	    await admin.save();
-		const token = await admin.generateAuthToken();
-		res.send({ admin,token});
-	} catch (err) {
-		res.status(400).send({message:'invalid user ditails'});
-	}
-});
+// const Admin = require('../models/adminModel')
+// const Book =require('../models/bookModel')
+// const router=new express.Router()
 
 
-router.post("/admin/login", async (req, res) => {
-	try {
-		 const admin = await Admin.findAdminbyEmailAndPassword(req.body.email, req.body.password);
-		 const token = await admin.generateAuthToken();
-		//res.cookie('token', token)
+// router.post("/admin/add" , async (req, res) => {
+//     const admin = new Admin(req.body);
+// 	console.log(admin)
+// 	try {
+// 	    await admin.save();
+// 		const token = await admin.generateAuthToken();
+// 		res.send({ admin,token});
+// 	} catch (err) {
+// 		res.status(400).send({message:'invalid user ditails'});
+// 	}
+// });
 
-		 res.send({admin,token})
+
+// router.post("/admin/login", async (req, res) => {
+// 	try {
+// 		 const admin = await Admin.findAdminbyEmailAndPassword(req.body.email, req.body.password);
+// 		 const token = await admin.generateAuthToken();
+// 		//res.cookie('token', token)
+
+// 		 res.send({admin,token})
 		
-	} catch (err) {
-		res.status(400).send({
-			status: 400,
-			message: 'unable to login!',
-		});
-	}
-});
+// 	} catch (err) {
+// 		res.status(400).send({
+// 			status: 400,
+// 			message: 'unable to login!',
+// 		});
+// 	}
+// });
 
-module.exports=router
+// module.exports=router
--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -1,110 +1,110 @@
-const express=require('express')
-const auth = require("../middleware/auth");
+// const express=require('express')
+// const auth = require("../middleware/auth");
 
-const User = require('../models/userModel')
-const Book =require('../models/bookModel')
-const router=new express.Router()
-
-
-
-router.post("/users/add", async (req, res) => {
-	const user = new User(req.body);
-	try {
-		await user.save();
-		const token = await user.generateAuthToken();
-		res.send({ user,token });
-	} catch (err) {
-		res.status(400).send({message:'invalid user ditails'});
-	}
-});
+// const User = require('../models/userModel')
+// const Book =require('../models/bookModel')
+// const router=new express.Router()
 
 
 
-router.post("/users/login", async (req, res) => {
-	try {
-		 const user = await User.findUserbyEmailAndPassword(req.body.email, req.body.password);
-		 const token = await user.generateAuthToken();
-		 const expirationTime=Date.now()+30000
-		 res.send({user,token,expirationTime})
+// router.post("/users/add", async (req, res) => {
+// 	const user = new User(req.body);
+// 	try {
+// 		await user.save();
+// 		const token = await user.generateAuthToken();
+// 		res.send({ user,token });
+// 	} catch (err) {
+// 		res.status(400).send({message:'invalid user ditails'});
+// 	}
+// });
+
+
+
+// router.post("/users/login", async (req, res) => {
+// 	try {
+// 		 const user = await User.findUserbyEmailAndPassword(req.body.email, req.body.password);
+// 		 const token = await user.generateAuthToken();
+// 		 const expirationTime=Date.now()+30000
+// 		 res.send({user,token,expirationTime})
 		
-	} catch (err) {
-		res.status(400).send({
-			status: 400,
-			message: 'unable to login!',
-		});
-	}
-});
+// 	} catch (err) {
+// 		res.status(400).send({
+// 			status: 400,
+// 			message: 'unable to login!',
+// 		});
+// 	}
+// });
 
-router.post("/users/logout", auth, async (req, res) => {
-	try {
-		req.user.tokens = req.user.tokens.filter((tokenDoc) => tokenDoc.token !== req.token);
-		await req.user.save();
-		res.send({data:req.user.name});
+// router.post("/users/logout", auth, async (req, res) => {
+// 	try {
+// 		req.user.tokens = req.user.tokens.filter((tokenDoc) => tokenDoc.token !== req.token);
+// 		await req.user.save();
+// 		res.send({data:req.user.name});
 		
-	} catch (err) {
-		res.status(500).send({msg:err});
-	}
-});
+// 	} catch (err) {
+// 		res.status(500).send({msg:err});
+// 	}
+// });
 
-router.post("/users/logoutAll", auth, async (req, res) => {
-	try {
-		req.user.tokens = [];
-		await req.user.save();
-		res.send();
-	} catch (err) {
-		res.status(500).send(err);
-	}
-});
+// router.post("/users/logoutAll", auth, async (req, res) => {
+// 	try {
+// 		req.user.tokens = [];
+// 		await req.user.save();
+// 		res.send();
+// 	} catch (err) {
+// 		res.status(500).send(err);
+// 	}
+// });
 
 
-router.post("/users/bookPurchase/:bookId",auth, async (req, res) => {
-	try {
-		const bookId=req.params.bookId;
-		const purchasedBook=Book.findById(bookId)
-		if(purchasedBook!=undefined){
-			req.user.purchasedBooks.push(bookId)
-			await req.user.save();
-			res.status(200).send(req.user.purchasedBooks)
-		}
-		else{
-			res.status(404).send({
-				status: 404,
-				message: 'book not found'
-			});
-		}
+// router.post("/users/bookPurchase/:bookId",auth, async (req, res) => {
+// 	try {
+// 		const bookId=req.params.bookId;
+// 		const purchasedBook=Book.findById(bookId)
+// 		if(purchasedBook!=undefined){
+// 			req.user.purchasedBooks.push(bookId)
+// 			await req.user.save();
+// 			res.status(200).send(req.user.purchasedBooks)
+// 		}
+// 		else{
+// 			res.status(404).send({
+// 				status: 404,
+// 				message: 'book not found'
+// 			});
+// 		}
        
-	} catch (err) {
-		res.status(400).send({
-			status: 400,
-			message: 'must be logged in!',
-		});
-	}
-});
+// 	} catch (err) {
+// 		res.status(400).send({
+// 			status: 400,
+// 			message: 'must be logged in!',
+// 		});
+// 	}
+// });
 
 
-router.post("/users/bookUnPurchase/:bookId",auth, async (req, res) => {
-	try {
-		const bookId=req.params.bookId;
-		const unPurchasedBook=Book.findById(bookId)
-		if(req.user.purchasedBooks.length>0){
-			req.user.purchasedBooks.splice(req.user.purchasedBooks.indexOf(bookId),1)
-			await req.user.save();
-			res.status(200).send(req.user.purchasedBooks)
-		}
-		else{
-			res.status(404).send({
-				status: 404,
-				message: 'book not found'
-			});
-		}
+// router.post("/users/bookUnPurchase/:bookId",auth, async (req, res) => {
+// 	try {
+// 		const bookId=req.params.bookId;
+// 		const unPurchasedBook=Book.findById(bookId)
+// 		if(req.user.purchasedBooks.length>0){
+// 			req.user.purchasedBooks.splice(req.user.purchasedBooks.indexOf(bookId),1)
+// 			await req.user.save();
+// 			res.status(200).send(req.user.purchasedBooks)
+// 		}
+// 		else{
+// 			res.status(404).send({
+// 				status: 404,
+// 				message: 'book not found'
+// 			});
+// 		}
        
-	} catch (err) {
-		res.status(400).send({
-			status: 400,
-			message: 'must be logged in!',
-		});
-	}
-});
+// 	} catch (err) {
+// 		res.status(400).send({
+// 			status: 400,
+// 			message: 'must be logged in!',
+// 		});
+// 	}
+// });
 
 
-module.exports=router
+// module.exports=router
