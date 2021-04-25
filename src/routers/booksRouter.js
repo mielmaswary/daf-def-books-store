ab759ecb3fc726554b4e4767f32803af1398e0ec
mielmaswary <miel1983@gmail.com>
2021-04-25 21:53:48 +0300

    try3

--- src/routers/booksRouter.js
+++ src/routers/booksRouter.js
@@ -32,100 +32,100 @@ router.get('books/get/:id', async (req,res)=>{
  
 
 
-router.get('books/get-by-id', auth, async (req,res)=>{
-    const purchasedsBooksIds=req.user.purchasedBooks
-    try{
-        console.log('books!')
-        const books=[]
-        if(purchasedsBooksIds.length>0){
-           for(let bookId of purchasedsBooksIds){
-                let book= await Book.findById(bookId)
-                books.push(book)
-           }
+// router.get('books/get-by-id', auth, async (req,res)=>{
+//     const purchasedsBooksIds=req.user.purchasedBooks
+//     try{
+//         console.log('books!')
+//         const books=[]
+//         if(purchasedsBooksIds.length>0){
+//            for(let bookId of purchasedsBooksIds){
+//                 let book= await Book.findById(bookId)
+//                 books.push(book)
+//            }
 
-        }
-        res.send(books)
-        console.log(books)
+//         }
+//         res.send(books)
+//         console.log(books)
 
-    }catch (err) {
-		res.status(400).send({
-			status: 400,
-			message: 'must be logged in!',
-		});
-	}
-})
+//     }catch (err) {
+// 		res.status(400).send({
+// 			status: 400,
+// 			message: 'must be logged in!',
+// 		});
+// 	}
+// })
 
-router.get('books/:searchValue', async (req,res)=>{
-    const searchValue=req.params.searchValue;
-    const regex = new RegExp(searchValue, 'i') // i for case insensitive
-     try{
+// router.get('books/:searchValue', async (req,res)=>{
+//     const searchValue=req.params.searchValue;
+//     const regex = new RegExp(searchValue, 'i') // i for case insensitive
+//      try{
 
-        const booksByBookName=await Book.find({bookName: {$regex: regex}})
-        const booksByAuthorName=await Book.find({authorName: {$regex: regex}})
-        books=booksByBookName.concat(booksByAuthorName)
-        if(!books)
-        {
-            res.status(404).send({'message':'no books to display'})
-        }
-        res.send(books)
-     }catch(err){
-        res.status(400).send(err.message)
-     }
- })
-
-
+//         const booksByBookName=await Book.find({bookName: {$regex: regex}})
+//         const booksByAuthorName=await Book.find({authorName: {$regex: regex}})
+//         books=booksByBookName.concat(booksByAuthorName)
+//         if(!books)
+//         {
+//             res.status(404).send({'message':'no books to display'})
+//         }
+//         res.send(books)
+//      }catch(err){
+//         res.status(400).send(err.message)
+//      }
+//  })
 
 
-router.post('books/add',async (req,res)=>{
+
+
+// router.post('books/add',async (req,res)=>{
     
-    const bookToAdd=new Book(req.body)
-    try{
-        if(!bookToAdd)
-        {
-            res.status(400).send({'message':'no books'})
-        }
+//     const bookToAdd=new Book(req.body)
+//     try{
+//         if(!bookToAdd)
+//         {
+//             res.status(400).send({'message':'no books'})
+//         }
       
-        await bookToAdd.save()
-        res.send(bookToAdd)
-    }catch(err){
-          res.status(500).send(err.message)
-    }
-})
+//         await bookToAdd.save()
+//         res.send(bookToAdd)
+//     }catch(err){
+//           res.status(500).send(err.message)
+//     }
+// })
 
-router.post('books/remove/:id',async (req,res)=>{
+// router.post('books/remove/:id',async (req,res)=>{
     
-    const bookToRemove=await Book.findById(req.params.id)
-    try{
-        if(!bookToRemove)
-        {
-            res.status(400).send({'message':'no book'})
-        }
-        await Book.findByIdAndDelete(bookToRemove._id)
-        res.send(bookToRemove)
-    }catch(err){
-          res.status(500).send(err.message)
-    }
-})
+//     const bookToRemove=await Book.findById(req.params.id)
+//     try{
+//         if(!bookToRemove)
+//         {
+//             res.status(400).send({'message':'no book'})
+//         }
+//         await Book.findByIdAndDelete(bookToRemove._id)
+//         res.send(bookToRemove)
+//     }catch(err){
+//           res.status(500).send(err.message)
+//     }
+// })
 
-router.patch('books/edit/:id', async(req,res)=>{
+// router.patch('books/edit/:id', async(req,res)=>{
 
-    const id=req.params.id
-    try{
-        const book=await Book.findByIdAndUpdate(id,req.body, {
-			new: true,
-			runValidators: true,
-		})
-        if (!book) {
-			return res.status(404).send({
-				status: 404,
-				message: "No book",
-			});
-		}
-		res.send(book);
+//     const id=req.params.id
+//     try{
+//         const book=await Book.findByIdAndUpdate(id,req.body, {
+// 			new: true,
+// 			runValidators: true,
+// 		})
+//         if (!book) {
+// 			return res.status(404).send({
+// 				status: 404,
+// 				message: "No book",
+// 			});
+// 		}
+// 		res.send(book);
       
-    }catch(err){
-          res.status(500).send(err.message)
-    }
-})
+//     }catch(err){
+//           res.status(500).send(err.message)
+//     }
+// })
 
 module.exports=router
