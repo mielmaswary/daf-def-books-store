bb4df9d8daa27930a74c869c026ca848104064f1
mielmaswary <miel1983@gmail.com>
2021-04-26 12:00:32 +0300

    try3

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 ///////////////////elements///////////////////////
 let expirationTime=0;
 let expirationTimeLeft=undefined;
-const hostUrl='https://miel-dafdef-book-store.herokuapp.com'
+const hostUrl='http://miel-dafdef-book-store.herokuapp.com'
 
 //header
 const utilsContainer=document.getElementsByClassName('utils-container')[0]
